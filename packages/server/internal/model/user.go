7169acc90d251ebbcf6006dc56086dcb1dce556a
noelorin <noelorin@outlook.com>
2026-05-30 02:29:57 +0800

    feat: user表新增avatar头像字段

--- packages/server/internal/model/user.go
+++ packages/server/internal/model/user.go
@@ -10,6 +10,7 @@ type User struct {
 	ID        uint   `gorm:"primaryKey" json:"id"`
 	UUID      string `gorm:"type:uuid;uniqueIndex" json:"uuid"`
 	Name      string `gorm:"uniqueIndex" json:"name"`
+	Avatar    string `gorm:"default:''" json:"avatar"`
 	Password  string `json:"-"`
 	Role      string `gorm:"default:user" json:"role"`
 	CreatedAt time.Time `json:"created_at"`
