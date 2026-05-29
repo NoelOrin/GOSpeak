927fa7b73d26465eea19eded0e6655fd4b4a43f1
noelorin <noelorin@outlook.com>
2026-05-29 17:12:29 +0800

    style: 移除Main组件背景半透明叠加色

--- packages/web/src/layouts/common/main.tsx
+++ packages/web/src/layouts/common/main.tsx
@@ -7,7 +7,7 @@ const Main = ({ children }: { children: JSX.Element; }) => {
 
   return (
     <div class="flex-1 w-full h-full select-none">
-          <div class="relative flex-1 bg-black/3.5 dark:bg-white/10 h-full">
+          <div class="relative flex-1 h-full">
             {children}
           </div>
     </div>
