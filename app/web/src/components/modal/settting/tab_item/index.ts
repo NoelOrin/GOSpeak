a94fce3427d79f78d6d303936208261d6912a930
noelorin <noelorin@outlook.com>
2026-07-07 23:21:39 +0800

    chore: remove empty general and room setting tabs

--- app/web/src/components/modal/settting/tab_item/index.ts
+++ app/web/src/components/modal/settting/tab_item/index.ts
@@ -1,12 +1,6 @@
 import audio from "./audio.tsx";
-// import general from "./general.tsx";
-// import room from "./room.tsx";
 import type { SettingTabConfig } from "./types";
 
-export const TABS: SettingTabConfig[] = [
-	// general,
-	// room,
-	audio,
-];
+export const TABS: SettingTabConfig[] = [audio];
 
 export type { SettingTabConfig };
