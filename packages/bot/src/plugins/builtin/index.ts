6a95ab49bb29abcfe4d0088314659ec5d18a26c5
noelorin <noelorin@outlook.com>
2026-07-09 15:57:15 +0800

    feat: add mute-manager builtin plugin

--- packages/bot/src/plugins/builtin/index.ts
+++ packages/bot/src/plugins/builtin/index.ts
@@ -2,3 +2,4 @@ export { RoomManagerPlugin } from "./room-manager";
 export { KeywordReplyPlugin } from "./keyword-reply";
 export { ModerationPlugin } from "./moderation";
 export { WelcomePlugin } from "./welcome";
+export { MuteManagerPlugin } from "./mute-manager";
